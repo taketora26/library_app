8e3a6149d1421845f7dc64c28ee157edb96b74df
taketora <utakaida@yahoo.co.jp>
2019-05-20 23:44:42 +0900

    不要なAbstractModuleの設定を削除

--- app/GuiceModule.scala
+++ app/GuiceModule.scala
@@ -1,15 +1,9 @@
-import akka.actor.ActorSystem
-import com.google.inject.{AbstractModule, Provides}
-import infra.rdb.{BookRepositoryOnJDBC, ExecutionContextOnJDBC}
+import com.google.inject.AbstractModule
+import infra.rdb.BookRepositoryOnJDBC
 import models.repositories.BookRepository
 
 class GuiceModule extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[BookRepository]).to(classOf[BookRepositoryOnJDBC])
   }
-  private val ecOnJDBC = new ExecutionContextOnJDBC(ActorSystem())
-
-  @Provides
-  def provideExecutionContextOnJDBC: ExecutionContextOnJDBC = ecOnJDBC
-
 }
